c650481d4d898056fe69f89e98e72755738c2e89
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-04-12 21:43:27 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,6 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init(e) {
+document.body.addEventListener('keydown',function init(e) {
  let i = 0;
  const key = parseInt(e.detail || e.which);
  
@@ -13,4 +13,4 @@ function init(e) {
    i = 0 ;
    
    }
-}
+})
