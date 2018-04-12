018ae8c4096b490190d0bf99c3c4110f6e217f77
wangfanqoo@126.com Wang <wangfanqoo@126.com>
2018-04-12 21:39:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,13 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
-function init() {
-  // Write your JavaScript code inside the init() function
+function init(e) {
+ let i = 0;
+ const key = parseInt(e.detail || e.which);
+ 
+ if(key === code[i]) {
+   i++;
+   if (i === code.length) {
+     alert("")
+   }
 
 }
